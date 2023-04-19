353bf267ddea407a3dc5f8706a84bae527508211
jun1491 <jun1491@nate.com>
2023-04-19 23:38:18 +0900

    init

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,74 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app"> 
+    <TodoHeader></TodoHeader>
+    <TodoInput v-on:addTodo="addTodo"></TodoInput>
+    <TodoList v-bind:propsdata="todoItems" @removeTodo="removeTodo"></TodoList>
+    <TodoFooter v-on:removeAll="clearAll"></TodoFooter>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import TodoHeader from './components/TodoHeader.vue'
+import TodoInput from './components/TodoInput.vue'
+import TodoList from './components/TodoList.vue'
+import TodoFooter from './components/TodoFooter.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+ 
+  data(){
+    return{
+      todoItems:[]
+    }
+  },
+  components: {  
+    'TodoHeader' : TodoHeader,
+    'TodoInput' : TodoInput,
+    'TodoList' : TodoList,
+    'TodoFooter' : TodoFooter
+  },methods:{
+      addTodo(todoItem){
+        console.log(todoItem);
+        localStorage.setItem(todoItem,todoItem);
+        this.todoItems.push(todoItem);
+
+      },
+      clearAll(){
+        localStorage.clear();
+        this.todoItems=[];
+      },
+      removeTodo(todoItem , index){
+        console.log('removeItem : ' + todoItem+" : " + index );
+        localStorage.removeItem(todoItem);
+
+        
+        this.todoItems.splice(index,1);
+        
+      }
+  },created(){
+    if(localStorage.length>0){
+      for(var i=0 ; i<localStorage.length ; i++){
+        this.todoItems.push(localStorage.key(i));
+      }
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+body{
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  background-color: #f6f6f8;
+}
+input{
+  border-style: groove;
+  width: 200px;  
+}
+button{
+  border-style: groove;  
+}
+.shadow{
+    box-shadow: 5px 10px 10px rgba(0,0,0,0.03);
 }
 </style>
---
+++ src/components/TodoInput.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <input type="text" v-model="newTodoItem" placeholder="입력하세요" v-on:keyup.enter="addTodo">
+    <button v-on:click="addTodo">추 가</button>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  data (){
+    return {
+      newTodoItem : ''
+    }
+  },
+  methods:{
+    addTodo(){
+      if(this.newTodoItem != ""){     
+        var val = this.newTodoItem && this.newTodoItem.trim();
+        this.$emit('addTodo' , val);  //localStorage.setItem(val , val);
+        this.clearInput();
+      }
+    },
+    clearInput(){
+      this.newTodoItem = '';
+    }
+
+  }
+}
+
+</script>
+
+<style scoped>
+input:focus{
+  outline: none;
+}
+.inputBox{
+  background:white;
+  height: 50px;
+  line-height: 50px;
+  border-radius: 5px;
+}
+.inputBox input{
+  border-style: none;
+  font-size:0.9em;  
+}
+
+.addContainer{  
+  float: right;
+  background: linear-gradient(to right,#6478FB,#8763FB);
+  display:inline-block;
+  border-radius: 0 5px 5px 0;
+}
+
+.addBtn{
+  color: white;
+  vertical-align: middle;
+}
+
+</style>
